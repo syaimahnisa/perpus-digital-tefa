d5fc85b8d9c821df3c372dd45d6efbec5f473aca
syaimahnisa <140956114+syaimahnisa@users.noreply.github.com>
2024-07-29 11:08:59 +0700

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container-fluid">
     <div class="row my-5">
-      <div class="col-lg-6">
+      <div class="col-lg-6 box">
         <nuxt-link to="../pengunjung/tambah">
           <div class="card bg-pengunjung rounded-5">
             <div class="card-body">
@@ -11,7 +11,7 @@
         </nuxt-link>
       </div>
 
-      <div class="col-lg-6">
+      <div class="col-lg-6 box">
         <nuxt-link to="/buku">
           <div class="card bg-buku rounded-5">
             <div class="card-body">
@@ -106,5 +106,8 @@ onMounted(() => {
 .card.bg-buku {
   background: url(../assets/img/bg-home-cari-buku.jpg) no-repeat center center;
   background-size: cover;
+}
+.box{
+    width: 45%;
 }
 </style>
